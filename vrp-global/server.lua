457cd9de7689a982a970d8589dd612af8fa744b3
Jonas <70606075+dokjons@users.noreply.github.com>
2021-02-12 10:55:46 +0100

    Changed Credit
    Changed credit message from chat message to pop out info box.

--- vrp-global/server.lua
+++ vrp-global/server.lua
@@ -39,8 +39,16 @@ AddEventHandler('chatMessage', function(source, name, msg)
       TriggerClientEvent('chatMessage', -1, "^4[Global Handling] " .. getIdentity(source).firstname .. " ".. getIdentity(source).lastname .. "^4: " .. '^7' .. string.sub(msg,8))
       PerformHttpRequest('https://discord.com/api/webhooks/772855204449615904/eehJjOKbCLZgq5CC6pd2VbCxi-MREIL3Ur6CwN-VB8ggdErB4LpL4WwzkW0G4rvdS0pV', function(err, text, headers) end, 'POST', json.encode({username = "[Global Handling]", content = "```["..user_idd.."]" .. " "..getIdentity(source).firstname.." "..getIdentity(source).lastname.. ": " ..msg.."```"}), { ['Content-Type'] = 'application/json' })
       CancelEvent()
-    elseif sm[1] == "/mecredit" then
+      elseif sm[1] == "/mecredit" then
       CancelEvent()
-      TriggerClientEvent('chatMessage', -1, "^4[Credits] ^7| ^6vrp-global by: ^7| ^3Github: dokjons ^7| ^2Discord: squiggles#0856" .. string.sub(msg,100))
+      TriggerClientEvent("pNotify:SetQueueMax", -1, "lmao", 1)
+      TriggerClientEvent("pNotify:SendNotification", -1, {
+      text = "<b style = 'color:whitesmoke'><center>vrp-global by:<br><center><b style = 'color:yellow'> Github: dokjons <br><center><b style = 'color:purple'> Discord: squiggles#0856<br>",
+      type = "info",
+      theme = "metroui",
+      queue = "lmao",
+      timeout = 3000,
+      layout = "bottomRight"
+      })
     end
 end)
